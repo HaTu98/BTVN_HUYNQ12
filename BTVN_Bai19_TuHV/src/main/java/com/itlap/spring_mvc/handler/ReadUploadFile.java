899b379aebefcb858576dd7a7ff3663c725aa6f1
TuHV <hatu98nd@gmail.com>
2019-04-26 14:11:04 +0700

    update

--- BTVN_Bai19_TuHV/src/main/java/com/itlap/spring_mvc/handler/ReadUploadFile.java
+++ BTVN_Bai19_TuHV/src/main/java/com/itlap/spring_mvc/handler/ReadUploadFile.java
@@ -13,16 +13,44 @@ public class ReadUploadFile {
 
     public Map ReadUploadFile(BufferedReader br) {
         try {
-            String line;
-            while ((line = br.readLine()) != null) {
-                String key = line.split(":")[0].trim();
-                try {
-                    String value = line.split(":")[1].trim();
-                    dict.put(key, value);
-                } catch (ArrayIndexOutOfBoundsException e) {
-                    dict.put(key, "");
+            String line = br.readLine();
+            if (!line.contains("@")) {
+                while (line != null) {
+                    String key = line.split(":")[0].trim();
+                    try {
+                        String value = line.split(":")[1].trim();
+                        dict.put(key, value);
+                    } catch (ArrayIndexOutOfBoundsException e) {
+                        dict.put(key, "");
+                    }
+                    line = br.readLine();
                 }
+            } else {
+                String key = "", value = "";
+                while (line != null) {
+                    if (line.contains("@")) {
+                        value = "";
+                        String ar[] = line.split("/");
+                        if (ar.length > 1) {
+                            key = ar[0].substring(1, ar[0].length()).trim();
+                            value = ar[1];
+                        }
+                    } else {
+                        value += "\n"+ line;
+                    }
 
+                    line = br.readLine();
+                    //System.out.println(key + " : " + value);
+                    System.out.println(line);
+                    if(line != null) {
+                        if (line.contains("@") && key != "") {
+                            dict.put(key, value);
+                        }
+                    }
+                }
+                if (key != "" && value != "") {
+                    dict.put(key, value);
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
